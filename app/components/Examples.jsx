8b40f0005b84bc9702531b8eb47af26773646c23
naffiq <abdu.galymzhan@gmail.com>
2017-01-23 23:37:00 +0600

    Updated text in examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 var Examples = (props) => {
   return(
     <div>
       <h1 className="text-center">Examples</h1>
-      <p>Welcome to examples page!</p>
+      <p>Here are a few example locations to try out:</p>
+      <ul>
+        <li><Link to="/?location=Almaty">Almaty</Link></li>
+        <li><Link to="/?location=Pavlodar">Pavlodar</Link></li>
+        <li><Link to="/?location=Saint%20Petersburgh">Saint Petersburgh</Link></li>
+      </ul>
     </div>
   );
 };
