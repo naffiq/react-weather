b246b2f8a0ef681a4a39129dc9f2c5fa57ecc5df
naffiq <abdu.galymzhan@gmail.com>
2017-01-23 18:46:42 +0600

    Added text to example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples Component</h3>
+      <h3>Examples Component!</h3>
+      <p>Welcome to examples page!</p>
     </div>
   );
 };
