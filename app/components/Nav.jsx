8dee960e5af3e607a8d94a779c0a49bfd16bf511
naffiq <abdu.galymzhan@gmail.com>
2017-01-23 23:04:18 +0600

    Styled nav component

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,11 +3,16 @@ import {Link, IndexLink} from 'react-router';
 
 var Nav = () => {
   return (
-    <div>
-      <h2>Nav component</h2>
-      <IndexLink to="/" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="dropdown menu" data-dropdown-menu>
+          <li className="menu-text">React Weather App</li>
+          <li><IndexLink to="/" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+          <li><Link to="/about" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+          <li><Link to="/examples" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+        </ul>
+      </div>
+
     </div>
   )
 };
