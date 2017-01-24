f919eed446834eeecd3567a9f841d53d9b5a1e17
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-01-24 18:49:04 +0600

    added error message modal window

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+var ErrorModal = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
+  },
+  getDefaultProps() {
+    return {
+      title: 'Error'
+    };
+  },
+  componentDidMount () {
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render () {
+    var {title, message} = this.props;
+
+    return (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p>
+          <button className="button hollow" data-close="">
+            Okay
+          </button>
+        </p>
+      </div>
+    );
+  }
+});
+
+module.exports = ErrorModal;
